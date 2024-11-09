f57ce58a50352533902559028f14c87fcf2551a9
iSaulZ <yanezsalazarsaul@gmail.com>
2024-11-08 20:10:52 -0600

    Add LoginCard component and form validation logic

--- siiaubutepic/src/App.jsx
+++ siiaubutepic/src/App.jsx
@@ -1,7 +1,9 @@
+import LoginCard from "./components/LoginCard";
+
 export default function App() {
   return (
-    <main className="dark text-foreground bg-background w-full h-screen">
-      
+    <main className="dark text-foreground bg-background w-full h-screen flex items-center justify-center">
+      <LoginCard />
     </main>
   )
 }
