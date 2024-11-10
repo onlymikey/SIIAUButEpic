f4ca5cae83fdab8bd071855c128cb6b93d65ad3f
onlymikey <angel.lopezb@proton.me>
2024-11-10 00:06:02 -0600

    Added gradient to bg and stylized the login card, also fixing some bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import LoginCard from "./components/LoginCard";
 
 export default function App() {
   return (
-    <main className="dark text-foreground bg-background w-full h-screen flex items-center justify-center">
+    <main className="dark text-foreground bg-gradient-to-bl from-slate-900 to-indigo-900 w-full h-screen flex items-center justify-center">
       <LoginCard />
     </main>
   )
