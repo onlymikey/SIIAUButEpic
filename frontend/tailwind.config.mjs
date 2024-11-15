f29f7da594d7463af7f82e9b2e430eb2009587d2
onlymikey <angel.lopezb@proton.me>
2024-11-15 01:32:50 -0600

    fixed tailwind config file

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const { nextui } = require('@nextui-org/react')
+import { nextui } from '@nextui-org/react'
 export default {
   content: [
     './index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
