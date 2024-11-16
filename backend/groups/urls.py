7b90b125139c491f7fb8ce11d62d0abbc48f5c4d
Jorge Palma <153771074+Palmast2@users.noreply.github.com>
2024-11-16 01:44:06 -0600

    API documentation ampliation: careers, groups, enrollments and classrooms

--- backend/groups/urls.py
+++ backend/groups/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('groups/', GroupListCreateView.as_view(), name='groups-list-create'),  # Listar y crear
     path('groups/<int:pk>/', GroupDetailView.as_view(), name='groups-detail'),  # Detalles, actualizar y eliminar
     path('groups/next-id/', GroupNextIdView.as_view(), name='groups-next-id'),  # Obtener el próximo ID
-    path('groups/create/', GroupCreateView.as_view(), name='groups-create'),  # Crear grupo y horarios
+    path('group/create/', GroupCreateView.as_view(), name='groups-create'),  # Crear grupo y horarios
 ]
