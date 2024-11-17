daa63722d598ed98a2d31d7e17965d5a686b3d66
Jorge Palma <153771074+Palmast2@users.noreply.github.com>
2024-11-17 14:57:14 -0600

    idioma de errores atualizado a español

--- backend/SIIAUButEpic/settings.py
+++ backend/SIIAUButEpic/settings.py
@@ -133,9 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
