e8651cd430ddc081c39da0f04f8931b1c838b808
Jorge Palma <153771074+Palmast2@users.noreply.github.com>
2024-11-18 00:25:35 -0600

    roles, alta y baja de users. Update is no required for update -ya funciona el is_active para interactuar solo con usuarios activos. -ya existe endpoint para dar de baja usuarios (solo admin puede) -ya existe endpoint para dar de alta usuarios (solo admin puede) -el create,update y delete solo puede hacerlo el admin -update ya no pide password

--- backend/auth_custom/serializers.py
+++ backend/auth_custom/serializers.py
@@ -4,7 +4,7 @@ from .models import CustomUser
 from django.core.exceptions import ValidationError
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = CustomUser
@@ -12,14 +12,19 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     # Sobrescribimos create para manejar el hash de la contraseña al crear un usuario
     def create(self, validated_data):
+        # Si no se proporciona contraseña, lanza un error
+        if 'password' not in validated_data or not validated_data['password']:
+            raise serializers.ValidationError({"password": "Este campo es requerido al crear un usuario."})
         validated_data['password'] = make_password(validated_data['password'])
-        return super(CustomUserSerializer, self).create(validated_data)
+        return super().create(validated_data)
 
     # También sobrescribimos update para el mismo propósito si se actualiza la contraseña
     def update(self, instance, validated_data):
         if 'password' in validated_data:
             validated_data['password'] = make_password(validated_data['password'])
-        return super(CustomUserSerializer, self).update(instance, validated_data)
+        else:
+            validated_data.pop('password', None)
+        return super().update(instance, validated_data)
     
     def validate(self, data):
         # Validación personalizada para el rol 'teacher'
