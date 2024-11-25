065e65293ae8e75a4cad60a14166dc54b53d6d23
Jorge Palma <153771074+Palmast2@users.noreply.github.com>
2024-11-25 11:59:13 -0600

    groups endpoints updated

--- backend/groups/urls.py
+++ backend/groups/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import GroupListView, GroupDetailView, GroupNextIdView, GroupCreateView
+from .views import GroupListView, GroupNextIdView, GroupCreateView, GroupDetailWithSchedulesView, GroupDeleteView, GroupUpdateView
 
 urlpatterns = [
     path('groups/', GroupListView.as_view(), name='groups-list'),  # Listar todos los grupos
-    path('groups/<int:pk>/', GroupDetailView.as_view(), name='groups-detail'),  # Detalles, actualizar y eliminar
     path('groups/next-id/', GroupNextIdView.as_view(), name='groups-next-id'),  # Obtener el próximo ID
     path('groups/create/', GroupCreateView.as_view(), name='groups-create'),  # Crear grupo y horarios
+    path('groups/<int:group_id>/schedules/', GroupDetailWithSchedulesView.as_view(), name='groups-schedules'),  # Detalles de un grupo con sus horarios
+    path('groups/<int:group_id>/delete/', GroupDeleteView.as_view(), name='groups-delete'),  # Eliminar un grupo y sus horarios
+    path('groups/<int:group_id>/update/', GroupUpdateView.as_view(), name='groups-update'),  # Actualizar un grupo y sus horarios
 ]
